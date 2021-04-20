c9ef4b070d331b383719cff7cccd21c03ad05883
nojiyoon <neutral.sth@gmail.com>
2021-04-20 15:43:52 +0900

    reupload

---
+++ run.sh
@@ -0,0 +1,17 @@
+# 실행 코드를 완성해주세요.
+python -m bert.run_squad \
+  --vocab_file=\
+  --bert_config_file=\
+  --init_checkpoint=\
+  --do_train=\
+  --train_file=\
+  --do_predict=\
+  --predict_file=\
+  --train_batch_size=8\
+  --learning_rate=3e-5\
+  --use_tpu=False \
+  --num_train_epochs=2 \
+  --max_seq_length=384 \
+  --doc_stride=128 \
+  --do_lower_case=False\
+  --output_dir=
