58f8f95e23620349c1cd6184272219d95af56975
JeightAn <jeightan@gmail.com>
2021-04-20 20:37:38 +0900

    [MOD] comment out refine_output method

--- korbert/run_squad.py
+++ korbert/run_squad.py
@@ -1031,18 +1031,18 @@ def get_final_text(pred_text, orig_text, do_lower_case):
 
   output_text = orig_text[orig_start_position:(orig_end_position + 1)]
   ######## JAEHAK 조사를 강제로 떼주는 방식 ##########
-  def refine_output(orig_answer, pred_answer):
-    while orig_answer in pred_answer:
-      if orig_answer not in pred_answer[:-1]:
-        break
-      pred_answer = pred_answer[:-1]
-    while orig_answer in pred_answer:
-      if orig_answer not in pred_answer[1:]:
-        break
-      pred_answer = pred_answer[1:]
-    return pred_answer
-  if orig_text in output_text:
-    output_text = refine_output(orig_text, output_text)
+  #def refine_output(orig_answer, pred_answer):
+  #  while orig_answer in pred_answer:
+  #    if orig_answer not in pred_answer[:-1]:
+  #      break
+  #    pred_answer = pred_answer[:-1]
+  #  while orig_answer in pred_answer:
+  #    if orig_answer not in pred_answer[1:]:
+  #      break
+  #    pred_answer = pred_answer[1:]
+  #  return pred_answer
+  #if orig_text in output_text:
+  #  output_text = refine_output(orig_text, output_text)
   ####################################################
   return output_text
 
